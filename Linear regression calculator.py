132fcc6b58bf5b2d453dec01e049b52a10f45bc3
Adam-Kotecki <a.kotecki11@gmail.com>
2023-01-06 23:53:53 +0100

    calculation of coefficients

--- Linear regression calculator.py
+++ Linear regression calculator.py
@@ -5,6 +5,10 @@ from tkinter.messagebox import showinfo
 from tkinter import filedialog
 import numpy as np
 import openpyxl
+from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 root = tk.Tk()
 
@@ -50,7 +54,7 @@ for i in range(10):
 
 frame.grid(column = 0, row = 3, columnspan = 2, sticky = "N")
 
-more = tk.Button(root, text="I have more values", command=lambda: get_data_from_xlsx())
+more = tk.Button(root, text="I have more values", command=lambda: more())
 more.grid(column=0, row = 4, columnspan = 2, sticky = "N")
 
 calculate = tk.Button(root, text="Calculate", command=lambda: get_data_from_widgets())
@@ -70,7 +74,21 @@ def get_data_from_widgets():
     
     x_list = np.array(x_list).reshape((-1, 1))
     y_list = np.array(y_list)
-    return x_list, y_list
+    calculate(x_list, y_list)
+
+
+def more():
+    root2 = tk.Tk()
+    root2.geometry('300x200')
+    root2.resizable(False, False)
+    root2.title('Get data from Excel')
+    root2.rowconfigure(0, weight=2)
+    root2.rowconfigure(1, weight=1)
+    info = tk.Label(root2, text="Please select file with format xlsx or xlsm. X values should be in the first column, and Y values in the second column. Do not use column headers. All data should be numeric", wraplength = 300, justify = tk.CENTER)
+    info.grid(column=0, row = 0)
+    select_file = tk.Button(root2, text="Select Excel file", command=lambda: [get_data_from_xlsx(), root2.destroy()])
+    select_file.grid(column=0, row = 1)
+    root2.mainloop()
 
 
 def get_data_from_xlsx():
@@ -81,11 +99,76 @@ def get_data_from_xlsx():
        tk.messagebox.showinfo(message="File with wrong format selected. Please select xlsx or xlsm.")
        return
     
+    x_list = []
+    y_list = []
     sheet = workbook.active
     
-
-def calculate():
-    pass
+    try:
+        r = 1
+        for cell in sheet['A']:
+            if (cell.value != None) and sheet.cell(row=r, column=2).value != None:
+                x_list.append(cell.value)
+                y_list.append(sheet.cell(row=r, column=2).value)
+                r += 1
+            else:
+                break
+    except:
+        tk.messagebox.showinfo(message="Something went wrong")
+        return
+    
+    x_list = np.array(x_list).reshape((-1, 1))
+    y_list = np.array(y_list)
+    calculate(x_list, y_list)
+    
+    
+def calculate(x, y):
+    model = LinearRegression()
+    model.fit(x, y)
+    r_sq = model.score(x, y)
+    b0 = model.intercept_
+    b1 = model.coef_
+    y_pred = b0 + b1 * x
+    
+    for widgets in root.winfo_children():
+      widgets.destroy()
+      
+    root.rowconfigure(0, weight=1)
+    root.rowconfigure(1, weight=6)
+    root.rowconfigure(2, weight=1)
+    root.rowconfigure(3, weight=1)
+    root.rowconfigure(4, weight=1)
+    root.rowconfigure(5, weight=1)
+    root.rowconfigure(6, weight=1)
+    
+    label_title = tk.Label(root, text="Calculated results:")
+    label_title.grid(row = 0)
+    
+    fig = Figure(figsize = (4, 4))
+    a = fig.add_subplot(111)
+    a.scatter(x, y) 
+    canvas = FigureCanvasTkAgg(fig, master = root)  
+    canvas.get_tk_widget().grid(row = 1)
+    canvas.draw()
+    
+    label_R2 = tk.Label(root, text="Coefficient of determination: " + str(r_sq))
+    label_R2.grid(row = 2)
+    
+    label_b0 = tk.Label(root, text="Intercept: " + str(b0))
+    label_b0.grid(row = 3)
+    
+    label_b1 = tk.Label(root, text="Intercept: " + str(b1))
+    label_b1.grid(row = 4)
+    
+    back = tk.Button(root, text="Go back", command=lambda: back())
+    back.grid(row = 6)
+    
+    def back():
+        pass
+    
+    print("r_sq: " + str(r_sq))
+    print("b0: " + str(b0))
+    print("b1: " + str(b1))
+    print("y_pred: " + str(y_pred))
 
 def learn():
     pass
